1d31edf6529f6b1453186a0e4c33b2ce6bc7e500
eriklimamolykote <eriklim@hotmail.com>
2023-08-10 16:09:29 -0300

    commit-pratica-bootstrap-10-08-2023

---
+++ free_bootstrap/erik_free_bootstrap/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Negócios Modernos v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licenciado sob MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// Este arquivo é intencionalmente branco
+// Use este arquivo para adicionar JavaScript para seu projeto
