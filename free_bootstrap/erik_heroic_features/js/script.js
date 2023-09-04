7a0191e27c192823d23e1b9445033d70881f5aa9
eriklimamolykote <eriklim@hotmail.com>
2023-09-04 10:05:23 -0300

    commit-pratica-bootstrap-04-09-2023

---
+++ free_bootstrap/erik_heroic_features/js/script.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/templaet/heroic-features)
+* Copyrgiht 2013-2023 Start Bootstrap
+* Licensed under MIT (http://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+*/
+// Este arquivo é intencionalmente branco
+// Use este arquivo para adicionar JavaScript ao seu projeto
